f497048d2e612442d599768cfcadb32db9624d95
aubembali <aubembali@gmail.com>
2022-11-30 08:17:10 +0000

    Injector components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { FaceSnap } from './models/face-snap.model.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
  
+  mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
+  
+  ngOnInit(){
+    this.mySnap = new FaceSnap(
+     "Archibald",
+     "Mon meilleur ami",
+      new Date(),
+      0,
+     'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+  
+    );
+
+    this.myOtherSnap = new FaceSnap(
+      "Archibald 2",
+      "Ma meilleure amie",
+       new Date(),
+       0,
+       'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+   
+    );
+
+    this.myLastSnap = new FaceSnap(
+      "Archibald 3",
+      "Ma meilleure amie 3",
+       new Date(),
+       0,
+       'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+   
+    );
+  }
 }
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -11,22 +11,10 @@ export class FaceSnapComponent implements OnInit {
   //injection du model
   @Input() faceSnap! : FaceSnap
 
-  title!:string;
-  description!:string;
-  createDate!:Date;
-  snap!:number;
-  imageUrl! :string;
-
   buttonText!:string;
 
   //Interface d'initialisation
   ngOnInit(){
-    this.title = "Archibald";
-    this.description = "Mon meilleur ami";
-    this.createDate = new Date();
-    this.snap = 6;
-    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
-
     this.buttonText = 'On Snap!';
   }
 
@@ -35,11 +23,11 @@ export class FaceSnapComponent implements OnInit {
 
 
     if (this.buttonText === 'On Snap!') {
-      this.snap ++;
+      this.faceSnap.snap ++;
       this.buttonText = 'Oops, unSnap ! ';
     } else {
 
-      this.snap --;
+      this.faceSnap.snap --;
       this.buttonText = 'On Snap!';
     }
   }
